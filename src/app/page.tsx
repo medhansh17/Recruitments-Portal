e077b4e7632cd371dde5325ceb30f355aaa34f43
Medhansh Jain <medhanshjain2010@gmail.com>
2024-03-19 21:43:30 +0530

    Developing domain joining page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,17 @@ import Loading from "../components/loading";
 import Hero from "@/components/hero";
 import ScrollIndicator from "@/components/scrollindicator";
 import Welcome from "@/components/welcome";
-
+import Joining from "@/components/joining-page";
 
 export default function Home() {
   return (
-  <main className="min-h-screen">
-    <Suspense fallback={<Loading/>}>
-        <Hero/>
-        <Welcome/>
-    </Suspense>
-    <ScrollIndicator/>
-    </main> 
+    <main className="min-h-screen">
+      <Suspense fallback={<Loading />}>
+        <Hero />
+        <Welcome />
+        <Joining />
+      </Suspense>
+      <ScrollIndicator />
+    </main>
   );
 }
---
+++ src/components/joining-page.tsx
@@ -0,0 +1,10 @@
+import Button from "./button";
+
+export default function Joining() {
+  return (
+    <div className="w-full h-screen">
+      <div></div>
+      <Button />
+    </div>
+  );
+}
