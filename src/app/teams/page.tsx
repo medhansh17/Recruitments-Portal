7f3c8c208afde4a03755104a082d70eba7a41ca6
Medhansh Jain <medhanshjain2010@gmail.com>
2024-03-24 19:31:13 +0530

    Resolved all type errors and optimized for production

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,13 +1,25 @@
 import JoinTeam from "@/components/joining-page";
 import ScrollIndicator from "@/components/scrollindicator";
 
-
-export default function Teams(){
-    return  <main className="min-h-screen">
-        <JoinTeam teamName="Management" order='1'/>
-        <JoinTeam teamName="Tech" order='2'/>
-        <JoinTeam teamName="Design" order='1'/>
-        <ScrollIndicator />
-
-        </main>
+export default function Teams() {
+  return (
+    <main className="min-h-screen">
+      <JoinTeam
+        teamName="Management"
+        order="1"
+        titles={["EVENTS", "OUTREACH", "FINANCE"]}
+      />
+      <JoinTeam
+        teamName="Tech"
+        order="2"
+        titles={["APP", "WEB", "AI/ML", "RnD", "DEVOPS"]}
+      />
+      <JoinTeam
+        teamName="Design"
+        order="1"
+        titles={["UI/UX", "GRAPHICS", "VIDEO"]}
+      />
+      <ScrollIndicator />
+    </main>
+  );
 }
--- src/components/subdomain-header.tsx
+++ src/components/subdomain-header.tsx
@@ -3,9 +3,9 @@
 import { useRef } from "react";
 import { motion, useInView } from "framer-motion";
 
-export default function Header(props: { title: string }) {
+const SubHeader = (props: { title: string }) => {
   const Ref = useRef(null);
-  const inView = useInView(Ref, { amount: 0.5, once: false });
+  const inView = useInView(Ref, { amount: 0.5, once: true });
   const animationOnViewLeft = {
     hidden: {
       scaleX: 0,
@@ -40,7 +40,7 @@ export default function Header(props: { title: string }) {
   };
   return (
     <motion.div
-      className="flex flex-col justify-evenly text-white font-striger w-max min-w-[350px]"
+      className="flex flex-col justify-evenly text-white font-striger w-max min-w-[346px]"
       ref={Ref}
     >
       <div className="flex flex-row justify-between">
@@ -50,7 +50,7 @@ export default function Header(props: { title: string }) {
           animate={inView ? "visible" : "hidden"}
           className="bg-[url('/heading-top-left.svg')] bg-contain bg-center bg-no-repeat w-[35%] origin-right"
         ></motion.span>
-        <span className="lg:text-6xl sm:text-4xl text-4xl">The</span>
+        <span className="lg:text-4xl sm:text-4xl text-2xl">The</span>
         <motion.span
           variants={animationOnViewRight}
           initial="hidden"
@@ -58,7 +58,7 @@ export default function Header(props: { title: string }) {
           className="bg-[url('/heading-top-right.svg')] bg-contain bg-center bg-no-repeat w-[35%] origin-left"
         ></motion.span>
       </div>
-      <div className="lg:text-8xl sm:text-6xl text-5xl mx-auto">
+      <div className="lg:text-6xl sm:text-5xl text-3xl mx-auto">
         {props.title}
       </div>
       <div className="flex flex-row justify-between">
@@ -68,7 +68,7 @@ export default function Header(props: { title: string }) {
           animate={inView ? "visible" : "hidden"}
           className="bg-[url('/heading-bottom-left.svg')] bg-contain bg-center bg-no-repeat w-[35%] origin-right"
         ></motion.span>
-        <span className="lg:text-6xl sm:text-4xl text-4xl">Team</span>
+        <span className="lg:text-4xl sm:text-4xl text-2xl">Team</span>
         <motion.span
           variants={animationOnViewRight}
           initial="hidden"
@@ -78,4 +78,6 @@ export default function Header(props: { title: string }) {
       </div>
     </motion.div>
   );
-}
+};
+
+export default SubHeader;
