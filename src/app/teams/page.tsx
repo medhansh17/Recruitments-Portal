c7bbb1b826929edaf53469632a969baee0bb3330
Medhansh Jain <medhanshjain2010@gmail.com>
2024-03-29 13:14:18 +0530

    Removed console logs - Completed Domain Page

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,32 +1,80 @@
+"use client";
 import JoinTeam from "@/components/joining-page";
 import ScrollIndicator from "@/components/scrollindicator";
 import Button from "@/components/button";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { GetDomains } from "@/api";
+import Loader from "@/components/loader";
 
 export default function Teams() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<any>({});
+
+  useEffect(() => {
+    const emailValue = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("email"))
+      ?.split("=")[1];
+    const accessToken = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("accessToken"))
+      ?.split("=")[1];
+
+    const fetchData = async () => {
+      setLoading(true);
+      if (!emailValue || !accessToken) {
+        alert("Email or Access Token not found in cookies");
+        setLoading(false);
+        return;
+      } else {
+        const data = await GetDomains(emailValue, accessToken);
+        setData(data);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <main className="min-h-screen">
-      <JoinTeam
-        teamName="Management"
-        order="1"
-        titles={["P&M", "Editotrial", "Events"]}
-      />
-      <JoinTeam
-        teamName="Tech"
-        order="2"
-        titles={["App", "Web", "AI/ML", "Research", "DevOps"]}
-      />
-      <JoinTeam
-        teamName="Design"
-        order="1"
-        titles={["UI/UX", "Graphic Design", "Video"]}
-      />
-      <div className=" flex justify-center items-center mb-36">
-        <Link href="/dashboard">
-          <Button text="Go to Dashboard" />
-        </Link>
-      </div>
-      <ScrollIndicator />
+      {loading ? (
+        <Loader visibility={true} /> // Display Loader while data is fetched
+      ) : (
+        <>
+          <JoinTeam
+            teamName="Management"
+            order="1"
+            titles={["P&M", "Editotrial", "Events"]}
+            selectedDomains={
+              data.management?.map((item: any) => item.subdomain) || []
+            }
+          />
+          <JoinTeam
+            teamName="Tech"
+            order="2"
+            titles={["App", "Web", "AI/ML", "Research", "DevOps"]}
+            selectedDomains={
+              data.tech?.map((item: any) => item.subdomain) || []
+            }
+          />
+          <JoinTeam
+            teamName="Design"
+            order="1"
+            titles={["UI/UX", "Graphic Design", "Video"]}
+            selectedDomains={
+              data.design?.map((item: any) => item.subdomain) || []
+            }
+          />
+          <div className=" flex justify-center items-center mb-36">
+            <Link href="/dashboard">
+              <Button text="Go to Dashboard" />
+            </Link>
+          </div>
+          <ScrollIndicator />
+        </>
+      )}
     </main>
   );
 }
