810f14d79433b5c7515b611ae3affa4460f36831
anshmehta7x <anshkapilmehta@gmail.com>
2024-03-29 21:15:17 +0530

    Quiz functionality removed, Horizontal Scroll on dashboard

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,48 +1,45 @@
 "use client";
 import Carousel from "@/components/carousel";
 import Image from "next/image";
+
 export default function Faq(){
 
     const FAQs:Array<{
-      question:string,
-      answer:string
+        question:string,
+        answer:string
     }> = [{
-      question:'WHat is IEEECS',
-      answer:"A 1 chapter"
-
+        question:'What qualifications do I need to join the technical chapter?',
+        answer:"Typically, we look for students with a strong interest and some background in technical fields such as computer science, engineering, or related disciplines. Specific requirements may vary, so please refer to our recruitment guidelines for details."
     },
-    {
-      question:'WHat is IEEECS',
-      answer:"A 2 chapter"
-
-    },
-    {
-      question:'WHat is IEEECS',
-      answer:"A 3 chapter"
-
-    },
-    {
-      question:'WHat is IEEECS',
-      answer:"A chapter"
-    },
-    {
-      question:'WHat is IEEECS',
-      answer:"A chapter"
-    },
-    {
-      question:'WHat is IEEECS',
-      answer:"A chapter"
-    },
-    {
-      question:'WHat is IEEECS',
-      answer:"A chapter"
-    }
-  ]
-
+        {
+            question:'What is IEEE-CS?',
+            answer:"IEEE-CS stands for Institute of Electrical and Electronics Engineers - Computer Society. It's a professional organization dedicated to advancing technology in the field of computer science and engineering. Our chapter at VIT focuses on fostering technical skills, networking opportunities, and professional development for students."
+        },
+        {
+            question:'What are the benefits of joining IEEE-CS at VIT?',
+            answer:"Joining IEEE-CS at VIT offers numerous benefits, including access to workshops, seminars, and conferences on the latest trends in technology. You'll also have the opportunity to collaborate on projects, network with industry professionals, and enhance your technical skills. Additionally, being a part of IEEE-CS can boost your resume and open up internship and job opportunities."
+        },
+        {
+            question:'How can I join IEEE-CS at VIT?',
+            answer:"To join IEEE-CS at VIT, you can participate in our recruitment process, which typically includes filling out an application, attending informational sessions, and completing tasks or quizzes to demonstrate your interest and skills. Keep an eye on our social media channels and website for updates on recruitment dates and requirements."
+        },
+        {
+            question:'What domains or activities does IEEE-CS at VIT offer?',
+            answer:"IEEE-CS at VIT offers a wide range of domains and activities to cater to different interests and skill sets. These may include software development, artificial intelligence, cybersecurity, data science, robotics, and more. As a member, you'll have the opportunity to explore these domains through workshops, projects, and hands-on learning experiences."
+        },
+        {
+            question:'Are there any membership fees for joining IEEE-CS at VIT?',
+            answer:"Yes, there may be nominal membership fees associated with joining IEEE-CS at VIT. These fees help support chapter activities, events, and resources provided to members. However, we strive to keep the fees affordable to ensure accessibility for all students. Detailed information about membership fees will be provided during the recruitment process."
+        },
+        {
+            question:'How can I stay updated with IEEE-CS events and activities at VIT?',
+            answer:"To stay updated with IEEE-CS events and activities at VIT, make sure to follow us on our social media channels, including Facebook, Instagram, and Twitter. You can also regularly check our website and subscribe to our newsletter for announcements, event schedules, and other important updates."
+        }
+    ]
     return <section className="flex flex-col w-screen items-center justify-center min-h-screen pt-16">
         <div className="flex flex-row items-center justify-center ">
           <div className="w-1/4 h-auto">
-          <img
+          <Image
             src="/graphics/left-lines.svg"
             alt="left-lines"
             width={500}
@@ -54,9 +51,9 @@ export default function Faq(){
             FAQ
           </h2>
           <div className="w-1/4 h-auto">
-          <img
+          <Image
             src="/graphics/right-lines.svg"
-            alt="left-lines"
+            alt="right-lines"
             width={500}
             height={100}
           />
--- src/components/faqcard.tsx
+++ src/components/faqcard.tsx
@@ -1,8 +1,37 @@
+import Image from 'next/image'
+
 export default function FaqCard(props:{indx:String,question:string,answer:string}){
-    return <div className="backdrop-blur-3xl flex flex-col bg-white h-[30vh]  md:h-[60vh] rounded-xl opacity-15 mt-10 mx-5  md:w-[20vw] border-main-blue border-8">
-        <h1 className="text-center">{props.indx}</h1><br></br>
-        {props.question}
-        {props.answer}
+    return <div
+        className="backdrop-blur-sm flex flex-col  h-[30vh]  md:h-[60vh] rounded-xl opacity-100 bg-white bg-opacity-10 mt-10 mx-5  md:w-[21vw] border-main-blue border-8">
+        <div className={"flex flex-row justify-center items-center mt-2"}>
+            <Image
+                src="/graphics/left-lines.svg"
+                alt="left-lines"
+                width={100}
+                height={100}
+            />
+            <h2 className="text-center font-striger text-white text-3xl mx-2">
+                {props.indx}
+            </h2>
+            <Image
+                src="/graphics/right-lines.svg"
+                alt="right-lines"
+                width={100}
+                height={100}
+            />
+        </div>
+
+        {/*question*/}
+        <section className={"font-striger text-xl text-white text-center md:min-h-[10vh]"}>
+            {props.question}
+
+        </section>
+        <br></br>
+
+        <section className={"font-sarpanch text-sm text-[#BBA8F2] text-center"}>
+            {props.answer}
+
+        </section>
     </div>
 
 }
